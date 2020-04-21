705f7208057c0d6c6118bd80fa474b4e725ed9e1
dvaknheo <dvaknheo@gmail.com>
2020-04-21 21:38:59 +0800

    添加文件

---
+++ app/Controller/api.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPHP
+ * From this time, you never be alone~
+ */
+namespace MY\Controller;
+
+use MY\Base\Helper\ControllerHelper as C;
+
+class api
+{
+    public function __construct()
+    {
+
+    }
+    public function user()
+    {
+        //$p=C::getParamge();
+        
+    }
+}
---
+++ app/Controller/api/info.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPHP
+ * From this time, you never be alone~
+ */
+namespace MY\Controller\api;
+
+use MY\Base\Helper\ControllerHelper as C;
+
+class info
+{
+    public function __construct()
+    {
+    }
+    public function v1()
+    {
+        //
+    }
+    public function v2()
+    {
+        //
+    }
+}
