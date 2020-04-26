9b269d1221691b4acb99cd288c0cfcfe5f2464fa
dvaknheo <dvaknheo@gmail.com>
2020-04-26 21:30:38 +0800

    就差 api/user 就剩下  blog 了

--- app/Controller/api/info.php
+++ app/Controller/api/info.php
@@ -14,10 +14,17 @@ class info
     }
     public function v1()
     {
-        //
+        C::header('Content-Type:application/xml; UTF-8');
+        echo <<<EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<response><status>success</status><data><version>1.0</version><author>yiisoft</author></data></response>
+
+EOT;
     }
     public function v2()
     {
-        //
+        // OK, the lazyest ^_^
+        C::header('Content-Type:application/json');
+        echo '{"status":"success","data":{"version":"2.0","author":"yiisoft"}}';
     }
 }
